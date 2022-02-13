fa8e647cabe0c0cda509ce0a9b7c564d7df38b8a
Giotje <devgio@icloud.com>
2022-02-13 15:08:28 +0000

    updated dotfiles

--- i3/scripts/main.sh
+++ i3/scripts/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1 --mode 1920x1080 --pos 2160x420 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1080x0 --rotate left --output DP-3 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1 --mode 1920x1080 --pos 2160x0 --rotate left --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1080x0 --rotate left --output DP-3 --off
